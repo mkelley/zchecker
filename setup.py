c74869cd7c5baf4bdab89be6cf8536a897c2ac5d
Michael S. P. Kelley <msk@astro.umd.edu>
2017-12-14 10:31:45 -0700

    New setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(name='zchecker',
+          version='0.1.0-dev',
+          description='ZTF moving target checker for short object lists',
+          author="Michael S. P. Kelley",
+          author_email="msk@astro.umd.edu",
+          url="https://github.com/mkelley/zchecker",
+          packages=find_packages(),
+          requires=['numpy', 'astropy', 'requests'],
+          license='BSD',
+      )
